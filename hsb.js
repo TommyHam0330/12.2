556a9c7fbe27499b7c826f550ef83b5704e4e488
kjr6975 <165114402+kjr6975@users.noreply.github.com>
2024-12-02 16:13:36 +0900

    task2 (#10)
    task2

--- hsb.js
+++
@@ -1,7 +0,0 @@
-let selectBox = document.getElementById("selectBox")
-let resultDiv = document.getElementById("result");
-
-selectBox.addEventListener("change", function() {
-    let select = selectBox.options[selectBox.selectedIndex].value;
-    resultDiv.innerText = "좋아하는 과일은: " + select;
-});
